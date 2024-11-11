7b83dd3310ec240d492f253ae77c5740ba881b5c
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-11-11 17:47:10 -0300

    Criando as tabelas

--- init.sql
+++ init.sql
@@ -1,5 +1,20 @@
+CREATE DATABASE IF NOT EXISTS test_db;
+
 USE test_db;
 
 --TODO Crie a tabela de user;
+CREATE TABLE users(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
 
 --TODO Crie a tabela de posts;
+CREATE TABLE posts(
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    user_id BIGINT,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
