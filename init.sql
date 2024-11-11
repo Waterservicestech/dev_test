7a9aa23669961868690c7c2e921149227d1d5371
Carloshpjacinto <carlo.henrique37@gmail.com>
2024-11-11 19:57:13 -0300

    feature/criacao-das-tabelas

--- init.sql
+++ init.sql
@@ -1,5 +1,20 @@
 USE test_db;
 
 --TODO Crie a tabela de user;
+CREATE TABLE user(
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
 
 --TODO Crie a tabela de posts;
+CREATE TABLE post(
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    userId VARCHAR(100) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
