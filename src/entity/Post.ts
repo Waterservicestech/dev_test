f7bff067d6a4b6d7bbec911520620347b8e21163
viniciuslemes10 <vinikjhgfds@gmail.com>
2024-11-11 17:48:03 -0300

    Criando a entidade de Post

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+import { User } from "./User";
 
 //TODO Crie a entidade de Post
+@Entity()
+export class Post {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ name: "title", type: "varchar", length: 100, nullable: false })
+    title!: string;
+
+    @Column({ name: "description", type: "varchar", length: 100, nullable: false })
+    description!: string;
+
+    @ManyToOne(() => User, (user) => user.posts)
+    @JoinColumn({name: "user_id"})
+    user!: User;
+}
