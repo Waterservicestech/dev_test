b2d1c7e4d33ba01a9f27961922e423a7759488b5
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2024-11-12 17:27:08 -0300

    Add user and post tables with corresponding entities

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,17 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 //TODO Crie a entidade de Post
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  title!: string;
+
+  @Column()
+  description!: string;
+
+  @Column()
+  userId!: number;
+}
