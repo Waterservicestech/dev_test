47380840c904ab575f51a0aa3315765a0018162f
MarcosKern <vkg.marcos@gmail.com>
2024-11-12 14:12:57 -0300

    Implement user & post entities

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm";
+import { User } from "./User"
 
-//TODO Crie a entidade de Post
+@Entity('post')
+export class Post{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length: 100, nullable: false})
+    title: string;
+
+    @Column({length: 100, nullable: false})
+    description: string;
+
+    @ManyToOne(() => User, (user) => user.id, onDelete: "CASCADE")
+    userId: User
+
+
+}
