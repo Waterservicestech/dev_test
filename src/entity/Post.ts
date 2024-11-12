c367d8dd61fd758a10b5665a95de17144e795d9f
Gabriel Andrey Michelotto <32842191+Aieriez@users.noreply.github.com>
2024-11-12 20:59:44 -0300

    Adicionado as atividades realizadas
    Signed-off-by: Gabriel Andrey Michelotto <32842191+Aieriez@users.noreply.github.com>

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,19 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
 
 //TODO Crie a entidade de Post
+@Entity()
+export class Post {
+    @PrimaryGeneratedColumn()
+    id?: number;
+    
+    @Column({type: "varchar", length: 100, nullable: false })
+    title!: string;
+    
+    @Column({type: "varchar", length: 100, nullable: false })
+    description: string | undefined
+    
+    @ManyToOne(() => User, (userId) => userId.posts, { nullable: false })
+    userId?: User;
+  
+}
