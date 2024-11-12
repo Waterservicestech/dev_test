94716ab6da0f22dda40a3ec986c499fa7d2f445d
henriquealexandre1373 <developer.henrique1373@gmail.com>
2024-11-11 22:40:59 -0300

    feat: add User and Post entities

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,38 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { User } from "./User";
 
-//TODO Crie a entidade de Post
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+    nullable: false,
+  })
+  title!: string;
+
+  @Column({
+    type: "varchar",
+    length: 100,
+    nullable: false,
+  })
+  description!: string;
+
+  @Column({ nullable: false })
+  userId!: string;
+
+  @ManyToOne(() => User, (user) => user.posts, {
+    nullable: false,
+    onDelete: "CASCADE",
+  })
+  @JoinColumn({ name: "userId" })
+  user!: User;
+}
