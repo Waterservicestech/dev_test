28c89142979a0b61bb7cab83eff3c157a45dfe23
pedrosteinmuller <pedrosteinmuller10105@hotmail.com>
2024-11-12 09:59:25 -0300

    feat: creating post and user entities

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,36 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
 
 //TODO Crie a entidade de Post
+import { User } from "./User";
+
+@Entity()
+export class Post {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    title: string;
+
+    @Column()
+    description: string;
+
+    @Column({ nullable: true })
+    userId?: number;    
+
+    // Relacionamento muitos-para-um: Muitos posts pertencem a um único usuário
+    @ManyToOne(() => User, (user) => user.posts)
+    user?: User; // O usuário que criou o post
+
+     // Construtor para inicializar as propriedades
+     constructor(
+        id: number,
+        title: string, 
+        description: string, 
+        userId?: number
+    ) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.userId = userId;
+    }
+}
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,36 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Post } from "./Post";
 
 //TODO Crie a entidade de User
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    // Relacionamento um-para-muitos: Um usuário pode ter muitos posts
+    @OneToMany(() => Post, (post) => post.user)
+    posts: Post[] = []; // A coleção de posts do usuário e inicializando posts com um array vazio.
+
+    // Construtor para inicializar as propriedades
+    constructor(
+        id: number,
+        firstName: string, 
+        lastName: string, 
+        email: string
+    ) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }    
+}
