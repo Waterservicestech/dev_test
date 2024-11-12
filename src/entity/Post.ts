9410a7153c7e1bd5dbe5a7d0740f9667f83f8c6f
gabriel acer <gabrielaug4@gmail.com>
2024-11-12 01:16:51 -0300

    feat: create 'User' and 'Post' entitys

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from "typeorm";
+import { User } from "./User";
 
 //TODO Crie a entidade de Post
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "varchar", length: 100 })
+  title: string;
+
+  @Column({ type: "varchar", length: 100 })
+  description: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  user: User;
+
+  constructor(title: string, description: string, user: User) {
+    this.title = title;
+    this.description = description;
+    this.user = user;
+  }
+}
