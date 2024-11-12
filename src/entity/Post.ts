3a7e6b223f9885487395462d6fbee814666b5341
edwordout <e-gomes@live.com>
2024-11-12 20:32:13 -0300

    Implementação do teste para vaga de Desenvolvedor Jr.

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,19 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 //TODO Crie a entidade de Post
+
+@Entity()
+export class Post {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ length: 100 })
+    title!: string;
+
+    @Column({ length: 100 })
+    description!: string;
+
+    @Column()
+    userId!: number;
+
+}
