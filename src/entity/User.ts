dfceaee1926106ac21e12c0fb77988ce0b647d13
mateushlsilva <mateushls01@gmail.com>
2024-11-12 15:44:18 -0300

    feature: add entity user and post

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Post } from "./Post";
 
 //TODO Crie a entidade de User
+@Entity({ name: "user" })
+export class User {
+    // define a chave primária como auto incremento
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false, length: 100 })
+    firstName: string;
+
+    @Column({ nullable: false, length: 100 })
+    lastName: string;
+
+    @Column({ nullable: false, length: 100 })
+    email: string;
+
+    @OneToMany(() => Post, (post) => post.userId)
+    post: Post[];
+}
