d30705cd18c7719599a6f2c894fabf49bdcfc34e
carvalhaus <ooliveira.joaop@gmail.com>
2024-11-12 16:09:44 -0300

    create entities User and Post

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Post } from "./Post";
 
 //TODO Crie a entidade de User
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ nullable: false, length: 100 })
+  firstName!: string;
+
+  @Column({ nullable: false, length: 100 })
+  lastName!: string;
+
+  @Column({ nullable: false, length: 100 })
+  email!: string;
+
+  @OneToMany(() => Post, (post) => post.user)
+  posts!: Post[];
+}
