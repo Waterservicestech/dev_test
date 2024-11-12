57249f067f333dae52c250b11e9bed9b58f3c87c
kelson camargo <kelson.camargo@kernellintelligence.com>
2024-11-12 16:28:15 -0300

    fix: init.sql | feat: builded entities

--- init.sql
+++ init.sql
@@ -1,22 +1,18 @@
 USE test_db;
 
--- Build a User table;
+-- Build a User table
 CREATE TABLE IF NOT EXISTS User (
   id        INT          AUTO_INCREMENT PRIMARY KEY,
   firstName VARCHAR(100) NOT NULL,
   lastName  VARCHAR(100) NOT NULL,
   email     VARCHAR(100) NOT NULL,
-  createdAt TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-  updatedAt TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
--- Build a Post Table;
+-- Build a Post Table
 CREATE TABLE IF NOT EXISTS Post (
   id          INT          AUTO_INCREMENT PRIMARY KEY,
   title       VARCHAR(100) NOT NULL,
   description VARCHAR(100) NOT NULL,
   userId      INT          NOT NULL,
-  createdAt   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP,
-  updatedAt   TIMESTAMP    DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   FOREIGN KEY (userId)     REFERENCES User(id) ON DELETE CASCADE
 );
