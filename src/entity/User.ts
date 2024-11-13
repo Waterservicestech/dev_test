d33e4c73444bd1958e6573c3822ba4885ae3f7ed
NoOneLevi <zxvitor1@hotmail.com>
2024-11-13 16:56:05 -0300

    feat: i've finished the technical test

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,16 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import Post from "./Post";
 
-//TODO Crie a entidade de User
+@Entity()
+export default class User {
+    @PrimaryGeneratedColumn()
+    id!:number
+    @Column({type: 'varchar', length: 100, nullable: false})
+    public firstName!: string
+    @Column({type: 'varchar', length: 100, nullable: false})
+    public lastName!: string
+    @OneToMany(() => Post, (post) => post.user)
+    posts?: Post[]
+    @Column({type: 'varchar', length: 100, nullable: false})
+    public email!: string
+}
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
 import express from 'express';
+import {config} from "dotenv";
 import { DataSource } from 'typeorm';
-import { User } from './entity/User';
-import { Post } from './entity/Post';
+import User  from './entity/User';
+import Post from './entity/Post';
+config()
 
 const app = express();
 app.use(express.json());
@@ -34,11 +36,46 @@ const initializeDatabase = async () => {
 initializeDatabase();
 
 app.post('/users', async (req, res) => {
-// Crie o endpoint de users
+  const { email, firstName, lastName } = req.body
+
+  if(!firstName || !lastName || !email) {
+    return res.status(404).json({message: "Missing required fields!"})
+  }
+
+  try {
+    const userRepository = AppDataSource.getRepository(User)
+    const newUser = userRepository.create({ firstName, lastName, email})
+    const savedUser = await userRepository.save(newUser)
+
+    return res.status(201).json(savedUser)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({message: "Error while creating user"})
+  }
 });
 
 app.post('/posts', async (req, res) => {
-// Crie o endpoint de posts
+  const {title, description, userId} = req.body
+  
+  if(!title || !description || !userId) {
+    return res.status(404).json({message: "Missing required fields!"})
+  }
+  try {
+    const postRepository = AppDataSource.getRepository(Post)
+    const userRepository = AppDataSource.getRepository(User)
+
+    const user = await userRepository.findOneBy({id: userId})
+    if(!user) {
+      return res.status(404).json({message: "User not exists!"})
+    }
+
+    const newPost = postRepository.create({title, description, user})
+    const savedPost = await postRepository.save(newPost)
+    res.status(201).json(savedPost)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({message: "Error while creating post"})
+  }
 });
 
 const PORT = process.env.PORT || 3000;
