eb5106f174e64d549fc1233d69e7b871afec08e1
rodrigo.correia <rodrigo.correia@laccan.ufal.br>
2024-11-12 23:02:02 -0300

    Creating the user and post endpoints

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { DataSource } from 'typeorm';
 import { User } from './entity/User';
 import { Post } from './entity/Post';
+import * as validator from "validator";
 
 const app = express();
 app.use(express.json());
@@ -35,10 +36,61 @@ initializeDatabase();
 
 app.post('/users', async (req, res) => {
 // Crie o endpoint de users
+
+  const {firstName, lastName, email} = req.body;
+
+  if(!firstName || !lastName || !email){
+    return res.status(400).json({error: 'Missing required fields'});
+  }
+
+  if(!validator.isEmail(email)) return res.status(400).json({error: 'Invalid email'});
+
+  try{
+    const user = new User();
+
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+
+    await AppDataSource.manager.save(user);
+
+    res.status(201).json(user); 
+    
+  }catch(error){
+    console.error(error);
+    res.status(500).json({error: 'Internal server error'});
+  }
+
 });
 
 app.post('/posts', async (req, res) => {
 // Crie o endpoint de posts
+
+  const {title, description, userId} = req.body;
+
+  if(!title || !description || !userId){
+    return res.status(400).json({error: 'Missing required fields'});
+  }
+
+  try{
+    const user = await AppDataSource.manager.findOneBy(User, {id: userId});
+
+    if(!user) return res.status(404).json({error: 'User not found'});
+
+    const post = new Post();
+
+    post.title = title;
+    post.description = description;
+    post.user = user;
+
+    await AppDataSource.manager.save(post);
+
+    return res.status(201).json(post);
+    
+  }catch(error){
+    console.error(error);
+    res.status(500).json({error: 'Internal server error'});
+  }
 });
 
 const PORT = process.env.PORT || 3000;
