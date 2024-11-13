288c73fba090f6b88ae09c59eab9983c94711de4
Luigi Ferreira Bertelli <berteluigi@gmail.com>
2024-11-13 19:01:41 -0300

    Correção de comentários e identação

--- src/index.ts
+++ src/index.ts
@@ -39,6 +39,7 @@ app.get('/users', async (req, res) => {
 });
 
 app.post('/users', async (req, res) => {
+  //Valida se a requisição possui todos os campos [firstName,lastName,email] definidos corretamente
   const {firstName, lastName, email} = req.body;
   if ((firstName === undefined || !firstName) ||
     (lastName === undefined || !lastName) ||
@@ -49,18 +50,21 @@ app.post('/users', async (req, res) => {
 
   try
   {
+    //Insere Usuário no banco de dados
     const userRepository = AppDataSource.getRepository(User);
     const user = userRepository.create({
       firstName: firstName,
       lastName: lastName,
       email: email
     });
-  
     const result = await userRepository.save(user);
+
+    //Retorna resultado da inserção com código 200
     return res.send(result);
   }
   catch (ex)
   {
+    //Loga o erro e retorna mensagem tratada para o consumido com o status 400
     console.log(ex);
     return res.status(400).send({success: false, message: "Não foi possível inserir o usuário no banco de dados, tente novamente!"});
   }
@@ -68,29 +72,34 @@ app.post('/users', async (req, res) => {
 });
 
 app.post('/posts', async (req, res) => {
+  //Valida se a requisição possui todos os campos [title,description,userId] definidos corretamente
   if ((req.body.title === undefined || !req.body.title) ||
-  (req.body.description === undefined || !req.body.description) ||
-  (req.body.userId === undefined || req.body.userId < 0))
+    (req.body.description === undefined || !req.body.description) ||
+    (req.body.userId === undefined || req.body.userId < 0))
   {
     return res.status(400).send({success: false, message: "Mensagem inválida!"});
   }
 
   try
   {
+    //Valida se Id do usuário informado realmente existe
     const userRepository = AppDataSource.getRepository(User);
     if(!(await userRepository.existsBy({id: req.body.userId})))
     {
       return res.status(400).send({message: "Usuário não encontrado"});
     }
 
+    //Insere Post no banco de dados
     const postRepository = AppDataSource.getRepository(Post);
     const post = postRepository.create(req.body);
-  
     const result = await postRepository.save(post);
+
+    //Retorna resultado da inserção com código 200
     return res.send(result);
   }
   catch (ex)
   {
+    //Loga o erro e retorna mensagem tratada para o consumidor com o status 400
     console.log(ex);
     return res.status(400).send({message: "Não foi possível inserir o post no banco de dados, tente novamente!"});
   }
