9107e19af3e86c2e0d9e825fe61342421178b182
PabloStrapasson <pablostrapasson98@gmail.com>
2024-11-12 21:13:23 -0300

    feat: :sparkles: User and Post entities, create users and posts tables on init.sql and dockerfile

--- init.sql
+++ init.sql
@@ -1,5 +1,30 @@
 USE test_db;
 
 --TODO Crie a tabela de user;
+CREATE TABLE IF NOT EXISTS "users" (
+    "id" int NOT NULL AUTO_INCREMENT,
+    "firstName" varchar(100) NOT NULL,
+    "lastName" varchar(100) NOT NULL,
+    "email" varchar(100) NOT NULL,
+    CONSTRAINT "user_pk" PRIMARY KEY ("id")
+);
+
+-- id – Tipo: Int, autoincremental, chave primária (PK).
+-- firstName – Tipo: Varchar(100), não nulo.
+-- lastName – Tipo: Varchar(100), não nulo.
+-- email – Tipo: Varchar(100), não nulo.
 
 --TODO Crie a tabela de posts;
+CREATE TABLE IF NOT EXISTS "posts" (
+    "id" int NOT NULL AUTO_INCREMENT,
+    "title" varchar(100) NOT NULL,
+    "description" varchar(100) NOT NULL,
+    "userId" int NOT NULL,
+    CONSTRAINT "PK_post" PRIMARY KEY ("id")
+    CONSTRAINT "FK_user" FOREIGN KEY ("userId") REFERENCES "users"("id")
+)
+
+-- id – Tipo: Int, autoincremental, chave primária (PK).
+-- title – Tipo: Varchar(100), não nulo.
+-- description – Tipo: Varchar(100), não nulo.
+-- userId – Tipo: Int, não nulo (chave estrangeira referenciando a tabela user)
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Post } from "./Post";
 
 //TODO Crie a entidade de User
+@Entity({ name: 'users'} )
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ name: 'firstName', length: 100, nullable: false })
+    firstName: string;
+
+    @Column({ name: 'lastName', length: 100, nullable: false })
+    lastName: string;
+
+    @Column({ name: 'email', length: 100, nullable: false })
+    email: string;
+
+    @OneToMany(() => Post, (post) => post.userId)
+    posts?: Post[]
+
+    constructor(firstName: string, lastName: string, email: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
+/*
+id – Tipo: Int, autoincremental, chave primária (PK).
+firstName – Tipo: Varchar(100), não nulo.
+lastName – Tipo: Varchar(100), não nulo.
+email – Tipo: Varchar(100), não nulo.
+*/
