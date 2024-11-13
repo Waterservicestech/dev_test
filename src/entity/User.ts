4a954176d16d3a69ff2ebffc6a2a6ba5bc5f42c6
eufelipelucio <eufeliplucio@gmail.com>
2024-11-13 00:55:29 -0300

    feat: create entity user and post

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
 
-//TODO Crie a entidade de User
+import { Post } from './Post';
+
+@Entity('user')
+export class User {
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ type: "varchar", length: 100, nullable: false })
+    firstName!: string;
+
+    @Column({ type: "varchar", length: 100, nullable: false })
+    lastName!: string;
+
+    @Column({ type: "varchar", length: 100, nullable: false })
+    email!: string;
+
+    @OneToMany(() => Post, (post) => post.user)
+    posts!: Post[];
+}
