3d1fe510db36e84ba02aab1a982bd577d10827ff
Paulo Caldi <114869399+pcaldi@users.noreply.github.com>
2024-11-13 20:59:14 -0300

    feat: implementado entity post

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,18 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToOne, JoinColumn, ManyToOne } from "typeorm";
+import { User } from "./User";
 
 //TODO Crie a entidade de Post
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ length: 100, nullable: false, type: "varchar" })
+  title!: string;
+
+  @Column({ length: 100, nullable: false, type: "varchar" })
+  description!: string;
+
+  @ManyToOne(() => User, (user) => user.posts)
+  user!: User;
+}
