3b10a76c527d5693045e43410fcbb68ad15559df
Erick Cassoli <erick.mcassoli@gmail.com>
2024-11-14 13:42:16 -0300

    feat(SQL): add user and post tables in init.sql
    - Define user table with id, firstName, lastName, email
    - Define post table with id, title, description, userId (FK)

--- init.sql
+++ init.sql
@@ -1,5 +1,17 @@
 USE test_db;
 
 --TODO Crie a tabela de user;
-
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY,
+    firstName VARCHAR(100) NOT NULL,
+    lastName VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL
+);
 --TODO Crie a tabela de posts;
+CREATE TABLE post (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(100) NOT NULL,
+    userId INT NOT NULL,
+    FOREIGN KEY (userId) REFERENCES user(id)
+);
