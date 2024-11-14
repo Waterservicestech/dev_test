a2e236e0d332f0671b1f8d9464b55241f97eb0ae
Gabriel Xavier Leite <133924991+X4vierL@users.noreply.github.com>
2024-11-13 22:29:02 -0300

    Alterações feitas seguindo os passos descritos.

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,55 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
 
-//TODO Crie a entidade de Post
+import User from "./User";
+
+@Entity()
+class Post {
+  @PrimaryGeneratedColumn("increment", { type: "int" })
+  private id!: number;
+
+  @Column({ type: "varchar", length: 100 })
+  private title!: string;
+
+  @Column({ type: "varchar", length: 100 })
+  private description!: string;
+
+  @Column({ type: "int" })
+  private userId!: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "userId" })
+  private user?: User;
+
+  // Get's
+  public getId(): number {
+    return this.id;
+  }
+  public getTitle(): string {
+    return this.title;
+  }
+  public getDescription(): string {
+    return this.description;
+  }
+  public getUserId(): number {
+    return this.userId;
+  }
+
+  // Set's
+  public setTitle(title: string): void {
+    this.title = title;
+  }
+  public setDescription(description: string): void {
+    this.description = description;
+  }
+  public setUserId(userId: number): void {
+    this.userId = userId;
+  }
+}
+
+export default Post;
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,44 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
-//TODO Crie a entidade de User
+@Entity()
+class User {
+  @PrimaryGeneratedColumn("increment", { type: "int" })
+  private id!: number;
+
+  @Column({ type: "varchar", length: 100, default: "" })
+  private firstName!: string;
+
+  @Column({ type: "varchar", length: 100, default: "" })
+  private lastName!: string;
+
+  @Column({ type: "varchar", length: 100, unique: true, default: "" })
+  private email!: string;
+
+  // Getters
+  public getId(): number {
+    return this.id;
+  }
+  public getFirstName(): string {
+    return this.firstName || "";
+  }
+  public getLastName(): string {
+    return this.lastName || "";
+  }
+
+  public getEmail(): string {
+    return this.email || "";
+  }
+
+  // Setters
+  public setFirstName(firstName: string): void {
+    this.firstName = firstName;
+  }
+  public setLastName(lastName: string): void {
+    this.lastName = lastName;
+  }
+  public setEmail(email: string): void {
+    this.email = email;
+  }
+}
+
+export default User;
