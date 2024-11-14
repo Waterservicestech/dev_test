e0b7bc60adf2624e292143f9a6d22bddf53694a5
VictorAndradeXD <54710922+VictorAndradeXD@users.noreply.github.com>
2024-11-14 19:03:05 -0300

    Dev_test-api

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,30 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from './User';
 
-//TODO Crie a entidade de Post
+@Entity('post')
+export class Post {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  title: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  description: string;
+
+  @ManyToOne(() => User, (user) => user.posts, { nullable: false })
+  @JoinColumn({ name: 'userId' })
+  user: User;
+
+  constructor(title: string, description: string, user: User) {
+    this.title = title;
+    this.description = description;
+    this.user = user;
+  }
+}
