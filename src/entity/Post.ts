aa5539a3466a8a519c0677dc306d41dcf63f66c5
pcaldi <pcaldi@gmail.com>
2024-11-14 15:29:00 -0300

    feat: implementado entity post e user

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, JoinColumn, ManyToOne } from "typeorm";
+import { User } from "./User";
 
 //TODO Crie a entidade de Post
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: "varchar", length: 100 })
+  title!: string;
+
+  @Column({ type: "varchar", length: 100 })
+  description!: string;
+
+  @Column()
+  userId!: number;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "userId" })
+  user!: User;
+}
