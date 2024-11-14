83abb7d5743d7eea3b7ae672195ce50190e7b4ff
Erick Cassoli <erick.mcassoli@gmail.com>
2024-11-14 15:31:52 -0300

    feat(entity): add Post entity
    - Created Post entity with fields: id, title, description, userId
    - Established relationship with User entity via userId

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,18 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-
 //TODO Crie a entidade de Post
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm';
+import { User } from './User';
+
+@Entity()
+export class Post {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ type: 'varchar', length: 100 })
+    title: string;
+
+    @Column({ type: 'varchar', length: 100 })
+    description: string;
+
+    @ManyToOne(() => User, (user) => user.posts)
+    user: User;
+}
