04a3ba4e8c16995b024cebc0aec81f2162082bc6
Bianca Schusarz <biancaschusarz@outlook.com>
2024-11-14 13:04:20 -0300

    Feat: funcionalidades Post e User e configuracao Docker

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,27 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn } from "typeorm"; 
+import { User } from "./User";
 
-//TODO Crie a entidade de Post
+/**
+ * A entidade Post representa um post no sistema e é mapeada para a tabela 'post' no banco de dados.
+ * 
+ * A classe contém os campos necessários para armazenar as informações de um post, como o título, a descrição,
+ * e a relação com o usuário que criou o post.
+ * 
+ * A entidade é associada a um usuário através da chave estrangeira 'userId', que refere-se à tabela de usuários.
+ */
+@Entity()
+export class Post {
+
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  title!: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  description!: string;
+
+  @ManyToOne(() => User, user => user.id, { onDelete: 'CASCADE' })
+  @JoinColumn({ name: "userId" })
+  user!: User;
+}
--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,23 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column } from "typeorm"; 
 
-//TODO Crie a entidade de User
+/**
+ * A entidade User representa um usuário no sistema e é mapeada para a tabela 'user' no banco de dados.
+ * 
+ * A classe contém os campos necessários para armazenar as informações de um usuário, como o nome, sobrenome 
+ * e o e-mail, que são dados essenciais para identificar um usuário na plataforma.
+ */
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  firstName!: string;
+
+  @Column({ type: 'varchar', length: 100 })
+  lastName!: string;
+
+  @Column({ type: 'varchar', length: 100, unique: true })
+  email!: string;
+}
