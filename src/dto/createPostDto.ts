c95bb2ab2ab3583dc0f8f5e211b4890e9a736009
Matheus Rosa <matheusrosataxa@gmail.com>
2024-11-15 10:23:53 -0300

    fix: createPostDto

--- src/dto/createPostDto.ts
+++ src/dto/createPostDto.ts
@@ -2,11 +2,11 @@ import { IsInt, IsString, Length } from 'class-validator';
 
 export class CreatePostDto {
   @IsString()
-  @Length(1, 255, { message: 'Title must be between 1 and 255 characters' })
+  @Length(1, 100, { message: 'Title must be between 1 and 100 characters' })
   title!: string;
 
   @IsString()
-  @Length(1, 1000, { message: 'Description must be between 1 and 1000 characters' })
+  @Length(1, 100, { message: 'Description must be between 1 and 1000 characters' })
   description!: string;
 
   @IsInt()
