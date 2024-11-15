7480226ddad4f2172bf4d11cf53f455b6f1bd3ef
thiagodeas <sousaalvesth@gmail.com>
2024-11-15 09:04:41 -0300

    Conclusão do Teste

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,25 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
 
 //TODO Crie a entidade de User
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Column({ length: 100 })
+    firstName: string;
+
+    @Column({ length: 100 })
+    lastName: string;
+
+    @Column({ length: 100 })
+    email: string;
+
+    constructor(firstName: string, lastName: string, email: string) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+    
+};
