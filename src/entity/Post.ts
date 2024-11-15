a4cf0ccacad0e09cf7872b3780bdf090a04d8f30
Laiz Nascimento de Oliveira <laizoliveira891@gmail.com>
2024-11-15 16:38:10 -0300

    Teste Desenvolvedor Back-End Laíz Nascimento

--- src/entity/Post.ts
+++ src/entity/Post.ts
@@ -1,3 +1,18 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
-
 //TODO Crie a entidade de Post
+
+class Post {
+    id: number;
+    titulo: string;
+    descricao: string;
+    user: User;
+
+
+    constructor(id: number, titulo: string, descricao: string, user: User) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.user = user;
+    }
+}
+
+export default Post;
