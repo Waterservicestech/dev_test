f379ccd3fb13a95d697e33e6d1bc07f8624f46f8
Fabio Penna <ffmpenna@gmail.com>
2024-11-15 03:51:31 -0300

    refactor: cria um arquivo separado para o banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,9 @@
 import 'reflect-metadata';
 import express from 'express';
-import { DataSource } from 'typeorm';
-import { User } from './entity/User';
-import { Post } from './entity/Post';
+import { AppDataSource } from './database';
 
 const app = express();
 app.use(express.json());
-
-const AppDataSource = new DataSource({
-  type: "mysql",
-  host: process.env.DB_HOST || "localhost",
-  port: 3306,
-  username: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "password",
-  database: process.env.DB_NAME || "test_db",
-  entities: [User,Post],
-  synchronize: true,
-});
 
 const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
