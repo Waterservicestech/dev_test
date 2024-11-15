5d3c4e5fa494133c8ed8240e8fd01066cc6c4df9
Macribeiro <matheusclaudinoribeiro@gmail.com>
2024-11-15 02:39:59 -0300

    Resoluçaõ dev_test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import 'reflect-metadata';
-import express from 'express';
-import { DataSource } from 'typeorm';
-import { User } from './entity/User';
-import { Post } from './entity/Post';
+import "reflect-metadata";
+import express from "express";
+import { DataSource } from "typeorm";
+import { User } from "./entity/User";
+import { Post } from "./entity/Post";
 
 const app = express();
 app.use(express.json());
@@ -14,11 +14,11 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "password",
   database: process.env.DB_NAME || "test_db",
-  entities: [User,Post],
+  entities: [User, Post],
   synchronize: true,
 });
 
-const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const initializeDatabase = async () => {
   await wait(20000);
@@ -33,12 +33,44 @@ const initializeDatabase = async () => {
 
 initializeDatabase();
 
-app.post('/users', async (req, res) => {
-// Crie o endpoint de users
+app.post("/users", async (req, res) => {
+  // Crie o endpoint de users
+  const { firstName, lastName, email } = req.body;
+
+  try {
+    const user = new User();
+    user.firstName = firstName;
+    user.lastName = lastName;
+    user.email = email;
+
+    let userRepository = AppDataSource.getRepository(User);
+    await userRepository.save(user);
+
+    res.status(201).json({ data: user });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error creating user" });
+  }
 });
 
-app.post('/posts', async (req, res) => {
-// Crie o endpoint de posts
+app.post("/posts", async (req, res) => {
+  // Crie o endpoint de posts
+  const { title, description, userId } = req.body;
+  try {
+    const post = new Post();
+
+    post.title = title;
+    post.description = description;
+    post.userId = userId;
+
+    let userRepository = AppDataSource.getRepository(Post);
+    await userRepository.save(post);
+
+    res.status(201).json({ data: post });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error generating post" });
+  }
 });
 
 const PORT = process.env.PORT || 3000;
