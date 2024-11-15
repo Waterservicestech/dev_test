e0c50824108dba8f8494ef6417496631070977d8
LiukenMonteiro <liuken37@gmail.com>
2024-11-15 12:44:35 -0300

    entidades 'User' e 'Post' criadas

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Post } from "./Post";
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number = 0;
+
+    @Column({type: "varchar", length: 100, nullable: false})
+    title: string = "";
+
+    @Column({type: "varchar", length: 100, nullable: false})
+    description: string = "";
+
+    @Column({type: "varchar", length: 100, nullable: false})
+    email: string = "";
+
+    @OneToMany(() => Post, (post) => post.user, {cascade: true})
+    posts: Post[] = [];
+}
 
 //TODO Crie a entidade de User
