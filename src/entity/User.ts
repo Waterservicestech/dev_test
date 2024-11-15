3954e7cae9d6ec48a1f550aa7da775a990689150
Fabio Penna <ffmpenna@gmail.com>
2024-11-15 03:20:02 -0300

    Add: Cria o arquivo de entidade de usuário.

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,21 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
+import { Post } from './Post';
 
 //TODO Crie a entidade de User
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id!: number;
+
+  @Column()
+  firstName!: string;
+
+  @Column()
+  lasName!: string;
+
+  @Column()
+  email!: string;
+
+  @OneToMany(() => Post, post => post.user)
+  posts!: Post[]
+}
